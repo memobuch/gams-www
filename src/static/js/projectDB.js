870f34d9939b45d5c7fafcfd87b34096717c35d0
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-01-28 11:36:21 +0100

    addeed a very basic search implementation

---
+++ src/static/js/projectDB.js
@@ -0,0 +1,132 @@
+
+window.gams = {}
+
+window.gams.projectDB = ((() => {
+
+    /**
+     * Abbreviation of the project
+     */
+    let PROJECT_ABBR;
+
+    /**
+     * Reference to the dexie database
+     */
+    let _DB;
+
+
+    /**
+     * TODO add database versioning 
+     * @param {string} projectAbbr
+     */ 
+    const init = (projectAbbr) => {
+        // somehow doesn't work with template string
+        let dexieDb = new Dexie(projectAbbr + "_db");
+        dexieDb.version(1).stores({
+            digital_objects: `
+                    db.id,
+                    *props.fulltext,
+                    *db.baseMetadata.title,
+                    *db.baseMetadata.description
+                `,
+        });
+        setDB(dexieDb);
+
+        PROJECT_ABBR = projectAbbr;
+        console.log("Initiating dexie database for project:  ", PROJECT_ABBR);
+
+
+        //////////////////////////////
+        ////
+        // TRYING TO ADD A FULTEXT INDEX at client side
+
+        // Add hooks that will index "message" for full-text search:
+        // db.digital_objects.hook("creating", function (primKey, obj, trans) {
+        //     console.log("Creating hook for", obj);
+        //     if (typeof obj.message == 'string') {
+        //         console.log("Creating hook for", obj);
+        //         obj.fulltextWords = getAllWords(obj.db.baseMetadata.description);
+        //     }
+        // });
+
+        // Add hooks that will index "message" for full-text search:
+        // getDB().digital_objects.hook("updating", function (mods, primKey, obj, trans) {
+        //     return { fulltextWords: getAllWords(obj.db.baseMetadata.description) };
+        // });
+    };
+
+    /**
+     * 
+     */
+    const populateDB = () => {
+        (async () => {
+
+            let projectJsonLocation = `/${PROJECT_ABBR}/object_index.json`;
+
+            // TODO surrond with try catch
+            const data = await fetch( projectJsonLocation).then(response => response.json());
+            await getDB().digital_objects.bulkPut(data);
+
+            console.log("Populated database with data from: ", data);
+            return data;
+        })();
+    };
+
+    
+    
+
+    function getAllWords(text) {
+        /// <param name="text" type="String"></param>
+        var allWordsIncludingDups = text.split(' ');
+        var wordSet = allWordsIncludingDups.reduce(function (prev, current) {
+            prev[current] = true;
+            return prev;
+        }, {});
+        return Object.keys(wordSet);
+    }
+
+
+    /**
+     * 
+     * @param {string} searchString 
+     */
+    const fulltextSearch = (searchString) => {
+
+        (async () => {
+            resultObjects = await getDB().digital_objects
+                .where("props.fulltext")
+                .startsWithIgnoreCase(searchString)
+                .toArray();    
+
+            console.log("Found objects for query:", searchString, resultObjects);
+
+            // TODO emit custom event?
+
+        })();
+
+        // TODO emit custom event?
+
+    }
+
+    const getDB = () => {
+        return _DB;
+    }
+
+    const setDB = (db) => {
+        _DB = db;
+    }
+
+
+
+    
+
+
+    return {
+        init,
+        populateDB,
+        fulltextSearch
+    };
+
+}))();
+
+// Expose the db object
+// window.gams.projectDB = projectDB;
