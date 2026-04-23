1a53f8b9e998743cc3ae504f527ee0cf799286ed
Leona Elisabeth Münzer <133358816+leonaelisabeth@users.noreply.github.com>
2026-04-23 12:53:57 +0200

    update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,13 +1,67 @@
 /* Service Worker */
 
 const VERSION = "v1";
+const CACHE_NAME = `meomor-${VERSION}`;
 
-// all the files that need to be chached for offline functionality
+// all the files that need to be cached for offline functionality
 const APP_STATIC_RESOURCES = [
   "/",
   "/",
   "/style.css",
   "/app.js",
-  "/",
+  "/icons/main_logo.jpg",
 ];
+
+
+// Saving the cache on PWA installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+// Updating the PWA and deleting old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+          return undefined;
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  // when seeking an HTML page
+  if (event.request.mode === "navigate") {
+    // Return to the index.html page
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  // For every other request type
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // Respond with a HTTP 404 response status.
+      return new Response(null, { status: 404 });
+    })(),
+  );
+});
 
