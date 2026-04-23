9cfd33db046e15edeb35cf94434709641f7801ba
Leona Elisabeth Münzer <133358816+leonaelisabeth@users.noreply.github.com>
2026-04-23 12:34:04 +0200

    update manifest.json, add sw.js

---
+++ src/sw.js
@@ -0,0 +1,13 @@
+/* Service Worker */
+
+const VERSION = "v1";
+
+// all the files that need to be chached for offline functionality
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/",
+  "/style.css",
+  "/app.js",
+  "/",
+];
+
